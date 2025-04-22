5d5335b68454726e024a27e7bb1109c13b74c841
Idealist <2046613303@qq.com>
2025-04-22 17:09:51 +0800

    SomePractice

--- Two.py
+++ Two.py
@@ -1,4 +1,14 @@
-sth= {'a': 1, 'b': 2, 'c': 3}
+# sth= {'a': 1, 'b': 2, 'c': 3}
 # print(sth.get('a',-11))
-sth1={1,2,3,3}
-print(sth1)
+# sth1={1,2,3,3}
+# print(sth1)
+
+def enroll(name, gender,sth='newBee'):
+    print('name:', name,'6')
+    print('gender:', gender)
+    print('sth:', sth)
+    print('\n')
+
+enroll(2,3)
+enroll(4,5,sth='666')
+enroll(6,7,'66666')
