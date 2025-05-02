3c8a2e7dbe8277fe2c1eca50b43d0c2d31ed14ad
Idealist <2046613303@qq.com>
2025-05-03 01:12:16 +0800

    practice 7.1

--- Two.py
+++ Two.py
@@ -3,12 +3,18 @@
 # sth1={1,2,3,3}
 # print(sth1)
 
-def enroll(name, gender,sth='newBee'):
-    print('name:', name,'6')
-    print('gender:', gender)
-    print('sth:', sth)
-    print('\n')
+# def enroll(name, gender,sth='newBee'):
+#     print('name:', name,'6')
+#     print('gender:', gender)
+#     print('sth:', sth)
+#     print('\n')
+#
+# enroll(2,3)
+# enroll(4,5,sth='666')
+# enroll(6,7,'66666')
 
-enroll(2,3)
-enroll(4,5,sth='666')
-enroll(6,7,'66666')
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+print(L[0])
+print(L)
+L=L[1:3]
+print(L)
