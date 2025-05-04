2d691f9b986b4cbf61872dcee3a1d7bec15f4bed
Idealist <2046613303@qq.com>
2025-05-05 02:02:42 +0800

    practice

--- Two.py
+++ Two.py
@@ -13,8 +13,39 @@
 # enroll(4,5,sth='666')
 # enroll(6,7,'66666')
 
-L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
-print(L[0])
-print(L)
-L=L[1:3]
-print(L)
+# L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+# print(L[0])
+# print(L)
+# L=L[1:3]
+# print(L)
+#
+# a=[x * x for x in range(1, 11)]
+# b= [x * x for x in range(1, 11) if x % 2 == 0]
+# c=[m + n for m in 'ABC' for n in 'XYZ']
+# d=[x if x % 2 == 0 else -x for x in range(1, 11)]
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# d = {'x': 'A', 'y': 'B', 'z': 'C' }
+# for k,v in d.items():
+#     print(k,'=',d[k])
+#     #print(k,'=',v)
+#
+# for k in d.items():
+#     print(k)
+#     #print(k,'=',v)
+#
+# for k,v in d.items():
+#     print(k,'=',d[k])
+#     #print(k,'=',v)
+
+# g = (x * x for x in range(10))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+def b():
+    yield 4
+c=b()
+print(next(c))
