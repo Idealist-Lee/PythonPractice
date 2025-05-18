0b06f8d58517301ac4e7aad9dc884e7faa7a0717
Idealist <2046613303@qq.com>
2025-05-19 01:30:31 +0800

    practice Numpy

---
+++ Numpy.py
@@ -0,0 +1,54 @@
+import numpy as np
+# L1 = np.array([1.,2,3,4,5])
+# L2 = np.array([1,2,3,4,5])
+# L2[0]=8.0
+# L1[2]=5
+# print(L1)
+# print(L2)
+# #
+# L1 = np.array([1,2,3,4,5])
+# print(L1)
+# L1=L1.astype(float)
+# print(L1)
+
+# L2 = np.array([1, 2, 3, 4, 5])
+# print(L2/1)
+# print(L2)
+
+# arr1=np.ones((1,5,5))
+# print(arr1)
+# print(arr1.shape)
+
+# arr1 = np.arange(10)
+# print(arr1)
+# arr2 = arr1.reshape( (1,-1) )
+# print(arr2)
+
+# arr2 = np.arange(10).reshape(2,5)
+# print(arr2)
+# arr1 = arr2.reshape( -1 )
+# print(arr1)
+
+# arr1 = np.arange(10)
+# print(arr1)
+
+# arr1 = np.arange(1,10,4)
+# print(arr1)
+
+# arr1 = np.zeros( 3 )
+# print(arr1)
+
+# arr2 = np.ones( (1,3) )
+# print(arr2)
+
+# arr1 = np.random.random( 5 )
+# print( arr1 )
+
+# arr2 = np.random.randint( 10,100,(1,15) )
+# print(arr2)
+
+# arr3 = np.random.normal( 0,1,(2,3) )
+# print( arr3 )
+
+arr4=np.random.randn(2,3)
+print(arr4)
--- Two.py
+++ Two.py
@@ -1,3 +1,14 @@
+# classmates = ['Michael', 'Bob', 'Tracy']
+# classmates.insert(1, 'Jack')
+# classmates.pop(1)
+# print(classmates)
+# print(classmates)
+# print(len(classmates))
+# print(classmates[0])
+
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+print(d['Michael'])
+
 # sth= {'a': 1, 'b': 2, 'c': 3}
 # print(sth.get('a',-11))
 # sth1={1,2,3,3}
@@ -13,11 +24,11 @@
 # enroll(4,5,sth='666')
 # enroll(6,7,'66666')
 
-# L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
-# print(L[0])
-# print(L)
-# L=L[1:3]
-# print(L)
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+print(L[0])
+print(L)
+L=L[1:3]
+print(L)
 #
 # a=[x * x for x in range(1, 11)]
 # b= [x * x for x in range(1, 11) if x % 2 == 0]
@@ -45,7 +56,7 @@
 # print(next(g))
 # print(next(g))
 # print(next(g))
-def b():
-    yield 4
-c=b()
-print(next(c))
+# def b():
+#     yield 4
+# c=b()
+# print(next(c))
